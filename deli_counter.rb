6833fdcaf8d88e165a7e028626e8dec4a8928744
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 09:18:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def "Deli Counter"
+  katz_deli = ["Logan", "Avi", "Spencer", "Amanda", "Annette", "Ruchi", "Jason", "Joe", "Rachel", "Lindsey"]
+  number += 0
+  take_a_number.each do |katz_deli|
+    puts "Welcome, #{katz_deli}. You are number #{number} in line."
+
+    
+  
