2bd5c7322c0174349cbc2041979c492301b287d6
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 09:37:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
-def "Deli Counter"
+def deli_counter
   katz_deli = ["Logan", "Avi", "Spencer", "Amanda", "Annette", "Ruchi", "Jason", "Joe", "Rachel", "Lindsey"]
   number += 0
   take_a_number.each do |katz_deli|
